7e5f929ba204038e0655daf49bee3710099b0c71
Sarah Schulte <git@sarah.engineer>
2022-05-18 20:27:18 -0700

    fix indentation

--- fix.py
+++ fix.py
@@ -1,12 +1,12 @@
 def fix_economy(people):
-fixit_fund = 0
+    fixit_fund = 0
     for person in people:
         fixit_fund += person.money
         person.money -= person.money
 
-for person in people:
-    if person == 'erikodes':
-        person.money += fixit_fund
-        fixit_fund = 0
+    for person in people:
+        if person == 'erikodes':
+            person.money += fixit_fund
+            fixit_fund = 0
 
-print('done')
+    print('done')
